f31724f12838f11ee323bb15208121e4db79dd3e
Pablo <p.reinaldo90@gmail.com>
2023-09-15 17:45:27 +0200

    delete dist

---
+++ dals/mock-data.js
@@ -0,0 +1,137 @@
+import { ObjectId } from "mongodb";
+export const db = {
+    houses: [
+        {
+            _id: new ObjectId().toHexString(),
+            description: "Casa Rural Malaga",
+            address: "Calle Primavera, 123, Malaga",
+            amenities: {
+                Habitaciones: 3,
+                camas: 6,
+                baños: 2,
+            },
+            reviews: [
+                {
+                    reviewerName: "Carlos",
+                    content: "Excelente lugar para descansar y disfrutar de la naturaleza.",
+                    rating: 5,
+                },
+                {
+                    reviewerName: "Ana",
+                    content: "Muy acogedora y bien equipada, perfecta para unas vacaciones en familia.",
+                    rating: 4,
+                },
+            ],
+        },
+        {
+            _id: new ObjectId().toHexString(),
+            description: "Casa Rural Tomelloso",
+            address: "Avenida del Sol, 456, Tomelloso",
+            amenities: {
+                Habitaciones: 4,
+                camas: 8,
+                baños: 3,
+            },
+            reviews: [
+                {
+                    reviewerName: "Luis",
+                    content: "Increíble estancia, todo muy limpio y en perfectas condiciones.",
+                    rating: 5,
+                },
+                {
+                    reviewerName: "Elena",
+                    content: "Buena ubicación y atención por parte del anfitrión.",
+                    rating: 4,
+                },
+            ],
+        },
+        {
+            _id: new ObjectId().toHexString(),
+            description: "Casa de campo Jaen",
+            address: "Calle del Olivo, 789, Jaen",
+            amenities: {
+                Habitaciones: 2,
+                camas: 4,
+                baños: 1,
+            },
+            reviews: [
+                {
+                    reviewerName: "Pablo",
+                    content: "Un lugar tranquilo y con hermosas vistas.",
+                    rating: 4,
+                },
+                {
+                    reviewerName: "Maria",
+                    content: "Ideal para desconectar del bullicio de la ciudad.",
+                    rating: 5,
+                },
+            ],
+        },
+        {
+            _id: new ObjectId().toHexString(),
+            description: "Casa con piscina",
+            address: "Calle del Lago, 101, Ciudad del Agua",
+            amenities: {
+                Habitaciones: 5,
+                camas: 10,
+                baños: 4,
+            },
+            reviews: [
+                {
+                    reviewerName: "Fernando",
+                    content: "La piscina es genial, pasamos unos días muy agradables.",
+                    rating: 4,
+                },
+                {
+                    reviewerName: "Laura",
+                    content: "Gran lugar para ir en grupo, tiene todo lo necesario para pasarla bien.",
+                    rating: 5,
+                },
+            ],
+        },
+        {
+            _id: new ObjectId().toHexString(),
+            description: "Apartamento con jardin",
+            address: "Calle de la Luna, 222, Pueblo Tranquilo",
+            amenities: {
+                Habitaciones: 1,
+                camas: 2,
+                baños: 1,
+            },
+            reviews: [
+                {
+                    reviewerName: "Isabel",
+                    content: "Pequeño pero muy acogedor, perfecto para una escapada en pareja.",
+                    rating: 4,
+                },
+                {
+                    reviewerName: "Andrés",
+                    content: "El jardín es encantador, disfrutamos mucho nuestra estancia.",
+                    rating: 5,
+                },
+            ],
+        },
+        {
+            _id: new ObjectId().toHexString(),
+            description: "Duplex en pleno centro",
+            address: "Avenida Central, 333, Ciudad Central",
+            amenities: {
+                Habitaciones: 3,
+                camas: 6,
+                baños: 2,
+            },
+            reviews: [
+                {
+                    reviewerName: "Marta",
+                    content: "La ubicación es inmejorable, cerca de todo.",
+                    rating: 5,
+                },
+                {
+                    reviewerName: "Pedro",
+                    content: "El dúplex es espacioso y cómodo, lo recomiendo.",
+                    rating: 4,
+                },
+            ],
+        }
+    ]
+};
